22553a3d821e11b2de69e07014caf62d02346423
calvear93 <calvear93@gmail.com>
2024-11-15 14:48:29 -0300

    chore: promise eslint

--- src/app/pages/detail/components/FetchBox.tsx
+++ src/app/pages/detail/components/FetchBox.tsx
@@ -8,11 +8,10 @@ export const FetchBox = withFeatures<FetchBoxProps>({
 	features: {
 		FEATURE_FETCHBOX_V2ALPHA: FetchBox_v2a,
 		// lazy load chunk
-		FEATURE_FETCHBOX_V1: lazy(() =>
-			import('./FetchBox.v1.tsx').then(({ FetchBox_v1 }) => ({
-				default: FetchBox_v1,
-			})),
-		),
+		FEATURE_FETCHBOX_V1: lazy(async () => {
+			const { FetchBox_v1 } = await import('./FetchBox.v1.tsx');
+			return { default: FetchBox_v1 };
+		}),
 	},
 	loading: <h2>Loading Feature</h2>,
 });
